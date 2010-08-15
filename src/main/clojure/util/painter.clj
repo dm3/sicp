f936cda175254cbd858a2235840dd14ce7fa6eb1
dm3 <deadmoro3@gmail.com>
2010-08-15 10:52:51 +0300

    working on graphics for painter

--- src/main/clojure/util/painter.clj
+++ src/main/clojure/util/painter.clj
@@ -1,8 +1,9 @@
 (ns util.painter
-  (:import (javax.swing JFrame JPanel JLabel JMenu JMenuBar JMenuItem)
+  (:import (javax.swing JFrame JPanel JLabel JMenu JMenuBar JMenuItem JComponent)
            (java.awt.event ActionListener MouseListener MouseAdapter)
            (java.awt GridLayout Color)))
 
+; My first try at swing
 (def exit-listener
   (proxy [ActionListener] []
     (actionPerformed [evt] (System/exit 0))))
@@ -25,25 +26,36 @@
                                (mouseDragged [e]
                                  (moveSquare (.getX e) (.getY e)))))))
 
+(defn read-image [path]
+  (javax.imageio.ImageIO/read (java.io.File. path)))
+
+(defn transforming-canvas [transform]
+  (doto (proxy [JComponent] []
+          (paint [g]
+            (doto g
+              (.setColor Color/WHITE)
+              (.fillRect 0 0 20 20)
+              (.setTransform transform)
+              (.setColor Color/BLACK)
+              (.drawRect 50 50 50 50)
+              (.fillOval 100 100 100 100))))
+    (.setOpaque true)
+    (.setDoubleBuffered true)))
+
 
 (defn panel-for [image]
   (add-mouse-listeners
     (doto (proxy [JPanel] [(GridLayout. 0 1)]
       (paintComponent [g]
-        (.drawString g "Custom!" 10 20)
-        (.setColor g Color/RED)
-        (.fillRect g 50 50 20 20)
-        (.setColor g Color/BLACK)
-        (.drawRect g 50 50 20 20)))
-       ;(.drawImage g image 0 0 g))))
+        (.drawImage g image 0 0 nil)))
       (.setBorder (javax.swing.BorderFactory/createLineBorder Color/BLACK))
       (.setBackground Color/RED)
-      (.setPreferredSize (java.awt.Dimension. 200 200))
+      (.setPreferredSize (java.awt.Dimension. 365 280))
       (.setOpaque false))))
 
 (defn in-frame [painter]
   (doto (JFrame. "Painter frame")
-    (.setContentPane (panel-for nil))
+    (.setContentPane (panel-for (read-image "src/main/resources/pic.jpg")))
     (.setJMenuBar menu)
     ;(.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
     (.pack)
