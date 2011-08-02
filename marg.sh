fa4857d75a2f677f7681beb97b4dccd15f0f0373
dm3 <deadmoro3@gmail.com>
2011-08-02 19:22:53 +0300

    Added mathjax

--- marg.sh
+++ marg.sh
@@ -3,5 +3,5 @@ mvn clean && mvn compile && mvn clojure:marginalia -Dmarginalia.outputType=multi
 git co gh-pages && rm -f *.html && \
 mv target/marginalia/toc.html index.html && \
 find target/marginalia -name '*.html' -exec sed -i 's/toc.html/index.html/' '{}' \; && \
-mv target/marginalia/*.html . && git add -u && \
-git ci -m "Updated generated documentation" && git co master
+mv target/marginalia/* . && git add -u && echo "Check for uncommitted html files, commit and switch to master..."
+#git ci -m "Updated generated documentation" && git co master
--- src/main/clojure/e1_11_12.clj
+++ src/main/clojure/e1_11_12.clj
@@ -6,11 +6,11 @@
 ;;
 ;; A function `f` is defined by the rule that
 ;;
-;; f(n) = n if n<3
+;; $$f(n) = n \mbox{ if } n<3$$
 ;;
 ;; and
 ;;
-;; f(n) = f(n - 1) + 2f(n - 2) + 3f(n - 3) if n > 3
+;; $$f(n) = f(n - 1) + 2f(n - 2) + 3f(n - 3) \mbox{ if } n > 3$$
 ;;
 ;; Write a procedure that computes `f` by means of a recursive process. Write a
 ;; procedure that computes `f` by means of an iterative process.
